5e265139dbaeebca4f73b8092e4162926c5b1a45
Sergey Darn <sergeydarn@gmail.com>
2025-04-14 18:07:23 +0300

    Refactor array utils to one line comprehensions

--- utils/array_utils.py
+++ utils/array_utils.py
@@ -3,25 +3,12 @@ from typing import List
 class ArrayUtils:
     @staticmethod
     def combine_arrays[T](array1: List[T], array2: List[T]):
-        res = array1
-        
-        for item in array2:
-            try:
-                res.index(item)
-            except:
-                res.append(item)
-                
-        return res
+        return array1 + [
+            item for item in array2 if (item not in array1)
+        ]
 
     @staticmethod
     def filter_array[T](array_to_filter: List[T], values_to_exclude: List[T]):
-        filtered_array = []
-        
-        for item in array_to_filter:
-            try:
-                values_to_exclude.index(item)
-            except:
-                filtered_array.append(item)
-                
-        return filtered_array
-    
+        return [
+            item for item in array_to_filter if (item not in values_to_exclude)
+        ]
