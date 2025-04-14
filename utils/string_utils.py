82b6eee5d8d236e8b7dde38cb8959973acb902f8
Sergey Darn <sergeydarn@gmail.com>
2025-04-14 12:27:20 +0300

    Handle empty urls edge cases

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -3,12 +3,18 @@ import re
 class StringUtils:
     @staticmethod
     def get_base_url(url: str) -> str:
+        if (not url):
+            return ""
+        
         matches = re.findall("https?\:\/\/[^/]*\/", url)
         
         return matches[0] if (len(matches) > 0) else ""
     
     @staticmethod
     def get_relative_url(url: str) -> str:
+        if (not url):
+            return ""
+
         base_url = StringUtils.get_base_url(url)
         
         url = url.replace(base_url, '').lstrip('/')
@@ -18,10 +24,16 @@ class StringUtils:
 
     @staticmethod
     def remove_get_parameters_and_hash(url: str) -> str:
+        if (not url):
+            return ""
+
         return re.sub("[?#][^\/]*$", "", url)
     
     @staticmethod
     def get_domain(url: str) -> str:
+        if (not url):
+            return ""
+
         base_url = StringUtils.get_base_url(url)
         domain = re.sub("https?:\/\/", "", base_url)
 
